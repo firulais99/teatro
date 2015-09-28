7e43aac51173918fd306cd80c9d2521701f7740d
AngelPat <Angelpat123@gmail.com>
2015-09-28 13:47:41 -0600

    creando migraciones

---
+++ database/migrations/2015_09_28_193658_create_Venta__table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVentaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Venta', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('Venta');
+    }
+}
